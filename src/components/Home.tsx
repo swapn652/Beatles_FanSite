bef096b7bb6a29079b3010dc9b9bfa5f6cc6f08f
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-06-30 17:00:59 +0530

    Final submission

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,17 +3,17 @@ import MemberCard from './MemberCard'
 const Home = () => {
   return (
     <>
-        <div className="flex flex-wrap ml-24">
-        <div className="w-1/2">
+        <div className="flex justify-center flex-wrap md:mr-32 mx-auto">
+        <div className="md:w-auto lg:w-1/2">
           <MemberCard name="John Lennnon" img_url="src/assets/members/john.png"/>
         </div>
-        <div className="w-1/2">
+        <div className="md:w-auto lg:w-1/2">
           <MemberCard name="Paul McCartney" img_url="src/assets/members/paul.png"/>
         </div>
-        <div className="w-1/2">
+        <div className="md:w-auto lg:w-1/2">
           <MemberCard name="George Harrison" img_url="src/assets/members/george.jpeg"/>
         </div>
-        <div className="w-1/2">
+        <div className="md:w-auto lg:w-1/2">
           <MemberCard name="Ringo Starr" img_url="src/assets/members/ringo.webp"/>
         </div>̀
       </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,18 +4,18 @@ export default function Navbar() {
     return (
       <>
         <nav className="bg-white ">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="w-full mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex items-center justify-between -mt-10 h-50">
               <div className="flex items-center">
                 <div className="flex-shrink-0">
-                  <img className="mt-10 -ml-16 h-30 w-60" src="src/assets/logo.jpeg" alt="Logo" />
+                  <img className="mt-10 md:h-24 lg:h-32 lg:w-60" src="src/assets/logo.jpeg" alt="Logo" />
                 </div>
                 <div className="hidden md:block">
-                  <div className="ml-10 mt-12 flex items-baseline space-x-4">
+                  <div className="mx-auto mt-12 flex items-baseline">
                     {/* <Link to='/'>yo</Link> */}
-                    <Link to="/" className="text-gray-600 hover:text-gray-800 px-8 py-2 rounded-md text-2xl">Home</Link>
-                    <Link to="/albums" className="text-gray-600 hover:text-gray-800 px-8 py-2 rounded-md text-2xl">Albums</Link>
-                    <Link to="/journey" className="text-gray-600 hover:text-gray-800 px-8 py-2 rounded-md text-2xl">Journey</Link>
+                    <Link to="/" className="text-gray-600 hover:text-gray-800 px-8 py-2 rounded-md md:text-xl lg:text-2xl">Home</Link>
+                    <Link to="/albums" className="text-gray-600 hover:text-gray-800 px-8 py-2 rounded-md md:text-xl lg:text-2xl">Albums</Link>
+                    <Link to="/journey" className="text-gray-600 hover:text-gray-800 px-8 py-2 rounded-md md:text-xl lg:text-2xl">Journey</Link>
                   </div>
                 </div>
               </div>
